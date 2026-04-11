08bd91c8c876c9a8fe17f4ba67ef94b448d1f275
mahendra011 <naman.mahendra@grofers.com>
2026-04-11 20:12:07 +0530

    Initial Commit

---
+++ Goalz/GoalzApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalzApp.swift
+//  Goalz
+//
+//  Created by Naman Mahendra on 11/04/26.
+//
+
+import SwiftUI
+
+@main
+struct GoalzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
